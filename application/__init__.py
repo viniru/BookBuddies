7e4f2d73032bdb424254609fd253f3e0d1130a2e
vinayak bhat <viniru100@gmail.com>
2019-07-25 00:17:59 +0530

    added comments

--- application/__init__.py
+++ application/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 import os
 
+#This function will be called when we type the command 'flask run' from the terminal
 
+#test_config can be used if we are using some different configuration during test, now
+#we are hardcoding as you can see (for database that is)
 def create_app(test_config=None,instance_relative_config=True):
 
-    app = Flask(__name__)
-    app.config.from_mapping(
+    app = Flask(__name__)   # create flask application
+    app.config.from_mapping(       #dev environment
         SECRET_KEY='dev'
     )
 
@@ -14,20 +17,27 @@ def create_app(test_config=None,instance_relative_config=True):
     app.config['MYSQL_USER'] = 'fhiZCqtsqe'
     app.config['MYSQL_PASSWORD'] = 'ff17M9rbwu'
     app.config['MYSQL_DB'] = 'fhiZCqtsqe'
-    app.database = MySQL(app)
+    app.database = MySQL(app)       #we assign the mysql object to app.database here, we cannot create the object after any
+                                    #request made, this will throw an error. We need to create in the beginning only
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
+    try:                                #when instance_relative_config = True (look at the parameter)
+        os.makedirs(app.instance_path)  #app.instance_path -> all the config details are stored. I dont know what config details
+    except OSError:                     #if we are using a local db, it would be stored here. 
         pass
 
-    from . import db
-    db.init_app(app)
+    from . import db                #We initialize app related stuff here -> go to db file for more details
+    db.init_app(app)                # we are basically calling the init_app() function from here by importing it
 
-    from . import auth
-    app.register_blueprint(auth.bp)
-
-    from . import view
+    from . import auth              # Here, auth contains some functions which can be classified as a view of specific type 
+    app.register_blueprint(auth.bp) # NOT MVC VIEW. Here, view is basically a set of related events that can happen
+                                     #like, authentication can be one view and you can have other such views
+                                     # here, view name is bp
+                                     #We are telling flask that, the view bp belongs to this particular object 'app', and when we 
+                                     # do this, by default , all the functions that route using 'bp' get registered to this 
+                                     # particular object
+                                
+                                   
+    from . import view               # same as before. This is another view
     app.register_blueprint(view.vw)
 
     return app
--- application/auth.py
+++ application/auth.py
@@ -4,12 +4,15 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 from application.db import get_db
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')        #we create a blueprint for a view here, named bp
+#The url of any function  that uses bp will have prefix of '/auth/ in this case
+                                                            
 
 
-@bp.route('/register')    
+@bp.route('/register')    # url would be /auth/register , it is bp.route and not app.route, bp is registered to app
 def register():
-    db = get_db()
+    db = get_db()           # gets the db connection that has already been made
     cur = db.connection.cursor()
     name = request.values["name"]
     password = request.values["password"]
