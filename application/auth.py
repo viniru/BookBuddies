92af02c323f155a28c3320969040710a49eba3d7
Raj <phantom@localhost.localdomain>
2019-07-25 00:19:37 +0530

    Updated authentication using POST method

--- application/auth.py
+++ application/auth.py
@@ -1,22 +1,22 @@
 import functools
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 from application.db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/register')    
+@bp.route('/register', methods=['POST'])    
 def register():
     db = get_db()
     cur = db.connection.cursor()
-    name = request.values["name"]
-    password = request.values["password"]
-    email = request.values["email"]
+    name = request.json["name"]
+    password = request.json["password"]
+    email = request.json["email"]
     cur.execute("INSERT INTO `User`(`name`, `password`, `email`) VALUES (%s, %s, %s)", (name, password, email))
     db.connection.commit()
-    return " "
+    return "Success!"
 
 
 
