77f4a859c6b9c1c5a6e941cb8cebb7cca25fc5e9
vinayak bhat <viniru100@gmail.com>
2019-08-08 02:43:06 +0530

    some polish

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 class App extends Component {
   state = {};
   render() {
-    return <h1> BOOK BUDDIES</h1>;
+    return <h1> Book Buddies </h1>;
   }
 }
 
