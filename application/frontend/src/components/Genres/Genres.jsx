e4b4a90e1426db6b53e0287442ccf9f4c1014ce0
vinayak bhat <viniru100@gmail.com>
2019-08-08 05:03:26 +0530

    home page

--- application/frontend/src/components/Genres/Genres.jsx
+++ application/frontend/src/components/Genres/Genres.jsx
@@ -1,8 +1,48 @@
+import "whatwg-fetch";
 import React, { Component } from "react";
 class Genres extends Component {
-  state = {};
+  state = {
+    count: 0,
+    genres: []
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:5000/genre/list")
+      .then(response => response.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            genres: result.response,
+            count: result.response.length
+          });
+        },
+
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+
+          console.log("error : " + error);
+        }
+      );
+  }
+
   render() {
-    return <button className="btn btn-primary m-4 btn-lg"> Genres </button>;
+    return (
+      <button onClick="" className="btn btn-primary m-4 btn-lg">
+        Genres({this.formatCount()})
+      </button>
+    );
+  }
+
+  formatCount() {
+    return this.state.count;
   }
 }
 
