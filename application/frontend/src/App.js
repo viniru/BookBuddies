039d27e14e6552709d837cb2df66823430394da5
vinayak bhat <viniru100@gmail.com>
2019-08-09 07:58:02 +0530

    modified the design

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -1,61 +1,10 @@
 import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import Books from "./components/Books/Books.jsx";
-import Friends from "./components/Friends/Friends.jsx";
-import Genres from "./components/Genres/Genres.jsx";
-import MyBooks from "./components/MyBooks/MyBooks.jsx";
+import Home from "./components/HomePage/Home.jsx";
 
 class App extends Component {
-  state = {
-    display: [1, 2, 3, 4],
-    everything: [1, 2, 3, 4]
-  };
-
-  checkVisibility = compo => {
-    return this.state.display.includes(compo);
-  };
-
-  displayAllComponent = () => {
-    var display = [...this.everything];
-    this.setState({ display });
-  };
-
-  activeComponent = compo => {
-    var display = [];
-    //display.push(compo);
-    this.setState({ display });
-  };
-
+  state = {};
   render() {
-    return (
-      <div>
-        <h1> Book Buddies</h1>
-        <Books
-          id={1}
-          visibility={this.checkVisibility(1)}
-          displayAllComponent={this.displayAllComponent}
-          activeComponent={this.activeComponent}
-        />
-        <MyBooks
-          id={2}
-          visibility={this.checkVisibility(2)}
-          displayAllComponent={this.displayAllComponent}
-          activeComponent={this.activeComponent}
-        />
-        <Friends
-          id={3}
-          visibility={this.checkVisibility(3)}
-          displayAllComponent={this.displayAllComponent}
-          activeComponent={this.activeComponent}
-        />
-        <Genres
-          id={4}
-          visibility={this.checkVisibility(4)}
-          displayAllComponent={this.displayAllComponent}
-          activeComponent={this.activeComponent}
-        />
-      </div>
-    );
+    return <Home />;
   }
 }
 
