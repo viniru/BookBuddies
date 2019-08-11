60889b1701d07e40ca2171e32e59f63a2d0be019
Saurabh Singh <singh.saurabh8527@gmail.com>
2019-08-11 17:40:34 +0530

    small change app.js

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-// import Books from "./components/Books/Books.jsx";
-// import Friends from "./components/Friends/Friends.jsx";
-// import Genres from "./components/Genres/Genres.jsx";
-import MyBooks from "./components/MyBooks/MyBooks.jsx";
 import Navbar from "./components/HomePage/navbar.jsx";
 
 class App extends Component {
@@ -13,7 +9,6 @@ class App extends Component {
       <div>
         <h1>Book Buddies</h1>
         <Navbar u_id={null} loggedIn={false}/>
-        <MyBooks />
       </div>
     );
   }
