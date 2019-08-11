05d31d1cd4a40cc601ff7dd15e4087d04d3b514b
vinayak bhat <viniru100@gmail.com>
2019-08-11 07:21:13 +0530

    Genre is working

--- application/frontend/src/components/Genres/Genres.jsx
+++ application/frontend/src/components/Genres/Genres.jsx
@@ -1,32 +1,84 @@
 import React, { Component } from "react";
-import GenresList from "./GenresList.jsx";
-
 class Genres extends Component {
   state = {
-    display: false
+    count: "0",
+    genres: [],
+    display: {
+      genres: false,
+      bookList: false
+    }
   };
 
-  openGenres = () => {
-    //this.props.activeComponent(this.props.id);
-    var display = true;
-    this.setState({ display });
+  formatCount() {
+    return this.state.count;
+  }
+
+  handleViewGenreBooks = event => {
+    this.setState({
+      genre_selected: event.target.value,
+      display: {
+        genres: false,
+        bookList: true
+      }
+    });
   };
+
+  componentDidMount() {
+    fetch("http://localhost:5000/genre/list")
+      .then(response => response.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            display: { genres: true },
+            genres: result.response,
+            count: result.response.length,
+            genre_selected: null
+          });
+        },
+
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+
+          console.log("error : " + error);
+        }
+      );
+  }
 
   render() {
-    return (
-      <React.Fragment>
-        {this.props.visibility && (
-          <button
-            onClick={this.openGenres}
-            className="btn btn-primary m-4 btn-lg"
-          >
-            Genres
-          </button>
-        )}
+    var container = {
+      width: "75%"
+    };
 
-        {this.state.display && <GenresList />}
-      </React.Fragment>
+    const genres = (
+      <div className={container}>
+        <div className="list-group">
+          {" "}
+          <br />
+          {this.state.genres.map(genre => (
+            <button
+              className="list-group-item list-group-item-primary btn btn-link"
+              type="button"
+              name={genre}
+              value={genre}
+              onClick={this.handleViewGenreBooks}
+              key={genre}
+            >
+              {genre}
+            </button>
+          ))}
+        </div>
+      </div>
     );
+
+    return this.state.display.genres ? (
+      genres
+    ) : this.state.display.bookList ? (
+      <h1>display the books here</h1>
+    ) : null;
   }
 }
 
